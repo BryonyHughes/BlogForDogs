77890ced53c33cb76d3d00426ee1574abe0233ea
BryonyHughes <bryony.hugs@gmail.com>
2020-12-23 14:42:40 +0000

    able to post comments to page

---
+++ app/Http/Controllers/CommentController.php
@@ -0,0 +1,117 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Comment;
+
+class CommentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+
+    public function page()
+    {
+      return view('comments.index');
+    }
+
+    public function apiIndex()
+    {
+      $comments = Comment::all();
+      return $comments;
+    }
+
+    public function apiStore(Request $request)
+    {
+      $validatedData = $request->validate([
+        'body'=>'required',
+      ]);
+
+      $c  = new Comment;
+      $c->body = $validatedData['body'];
+      $c->post_id =22 ;
+      $c->user_id = 5;
+      $c->save();
+
+      return response($c);
+
+    }
+
+
+
+
+}
