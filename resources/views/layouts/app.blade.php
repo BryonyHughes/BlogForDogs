7272f1301a3cc8960b7db4400dd1e849863ed833
BryonyHughes <bryony.hugs@gmail.com>
2020-12-29 23:37:34 +0000

    added notifications

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,16 +1,20 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
+
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
+
+
     <title>A Blog For Dogs  @yield('title')</title>
 
     <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
+  <script src="{{ asset('js/app.js') }}"defer></script>
+
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
@@ -18,6 +22,13 @@
 
     <!-- Styles -->
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" rel="stylesheet">
+    <link href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap-glyphicons.css" rel="stylesheet">
+
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+
+
+
 </head>
 <body>
     <div id="app">
@@ -49,6 +60,23 @@
                                 </li>
                             @endif
                         @else
+
+                        <li class="nav-item dropdown" id="markAsRead" onclick="markNotificationAsRead">
+                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
+                            data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre> <span class="glyphicon glyphicon-globe"></span>
+                                Notifications <span class="badge">{{count(auth()->user()->unreadNotifications)}}</span>
+                            </a>
+
+                            <ul class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                  <li>
+                                      @foreach (auth()->user()->unreadNotifications as $notification)
+                                      <a href = "{{ route('comments.page',$notification->data['post']['id']) }}">
+                                           {{ $notification->data['user']}} has commented on <strong> {{$notification->data['post']['title']}}</strong>
+                                      </a>
+                                      @endforeach
+                                  </li>
+                            </ul>
+                        </li>
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }}
@@ -72,9 +100,14 @@
             </div>
         </nav>
 
+        @if(session('message'))
+          <p><b>{{session('message')}}</b></p>
+        @endif
+
         <main class="py-4">
             @yield('content')
         </main>
     </div>
+    <script src ="{{asset('/js/app.js')}}"></script>
 </body>
 </html>
